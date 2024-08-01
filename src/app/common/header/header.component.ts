7a3356036836cdadff906581507cee7b99baa251
Hridoy, Arnob <hridoya@coned.com>
2024-08-01 00:13:21 -0400

    header updated with navigations

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isChartsActive = false;
+  isHomeActive = false;
+  isAddActive = false;
+  constructor( private router: Router){}
+  ngOnInit() {
+    const path = this.getPathname();
+    this.setActiveBar(path);
+  }
 
+  getPathname() : string {
+    return window.location.pathname.split('/')[1];
+  }
+
+  setActiveBar(value: string) {
+    this.isChartsActive = false;
+    this.isHomeActive = false;
+    this.isAddActive = false;
+
+    if (value === 'charts') {
+      this.isChartsActive = true;
+    } else if (value === ''){
+      this.isHomeActive = true;
+    } else {
+      this.isAddActive = true ;
+    }
+  }
+
+  routeTo(value: string): void {
+    this.setActiveBar(value);
+    this.router.navigate([value]);
+    const element  = document.getElementById('navbar');
+    element?.classList.remove('show')
+  }
 }
