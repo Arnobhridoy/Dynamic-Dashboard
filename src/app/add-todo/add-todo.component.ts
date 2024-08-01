adbf73c62ea3603a215791ac264da5a7294613f6
Hridoy, Arnob <hridoya@coned.com>
2024-08-01 01:25:13 -0400

    added form

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TodoService } from '../service/todo.service';
 
 @Component({
   selector: 'app-add-todo',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './add-todo.component.html',
   styleUrl: './add-todo.component.scss'
 })
 export class AddTodoComponent {
+  submitted = false;
 
+  todoForm: FormGroup = new FormGroup({
+    id:  new FormControl(''),
+    userId:  new FormControl(''),
+    title: new FormControl(''),
+    completed: new FormControl('')
+  });
+
+  constructor( 
+    private formBuilder: FormBuilder,
+    private todoService: TodoService
+  ) {}
+
+  ngOnInit(): void {
+    this.todoForm = this.formBuilder.group({
+      // TODOS: add validation
+      id: [''],
+      userId: [''],
+      titele: [''],
+      completed: [''],
+    });
+  }
+
+  onSubmit(): void {}
 }
