570095246912b6bba831034e51479cad0c93741b
Hridoy, Arnob <hridoya@coned.com>
2024-08-01 00:31:36 -0400

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ChartComponent } from './chart/chart.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DashboardComponent},
+    {path: 'charts', component: ChartComponent},
+    {path: 'add', component: AddTodoComponent}
+];
