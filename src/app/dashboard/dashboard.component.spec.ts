e8174d738af518e4cdceda59c7b06575b1e924e0
Hridoy, Arnob <hridoya@coned.com>
2024-08-01 01:07:12 -0400

    updated unit test

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { TodoService } from '../service/todo.service';
+import { HttpClientModule, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { HttpClientTestingModule,  } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { BehaviorSubject, Observable, Subject, of, throwError } from 'rxjs';
+
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let todoServiceSpy: jasmine.SpyObj<TodoService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [
+        DashboardComponent,
+        HttpClientTestingModule,
+        HttpClientModule,
+      ]
     })
     .compileComponents();
 
@@ -19,5 +31,11 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getTodos on initialization', () => {
+    spyOn(component, 'getTodos');
+    component.ngOnInit();
+    expect(component.getTodos).toHaveBeenCalled();
   });
 });
