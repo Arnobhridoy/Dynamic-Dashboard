65d2ded04d2cff456cc883817e833e0ef2cc8e16
Hridoy, Arnob <hridoya@coned.com>
2024-08-01 00:53:54 -0400

    updated dashboard component to call api

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Todo } from '../common/models/todo.model';
+import { TodoService } from '../service/todo.service';
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [TableModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  todoData: Todo[] = []
 
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit(): void {
+    this.getTodos();
+  }
+
+  getTodos(): void {
+    this.todoService.getTodos().subscribe({
+      next: (response: Todo[]) => {
+        this.todoData = response;
+      },
+      error: (err: any) => {
+        console.error(err)
+      }
+    })
+  }
 }
